7966107e1c86dbed1a715192134a70d7306db573
Jack <jackubc2013@gmail.com>
2018-01-14 12:56:45 -0800

    Added flexibility to sms messages received to update db

--- functions/__main__.js
+++ functions/__main__.js
@@ -5,17 +5,24 @@ const updateVote = require('./updateVote');
  * @param {string} sender The phone number that sent the text to be handled
  * @param {string} receiver The StdLib phone number that received the SMS
  * @param {string} message The contents of the SMS
- * @returns {string}
+ * @returns {any}
  */
 module.exports = (sender, receiver, message, context, callback) => {
+
+    // Message is in the format "listing_id voter_id applicant_id vote"
+    const params = message.split(' ');
+    const listing_id = parseInt(params[0]);
+    const voter_id = parseInt(params[1]);
+    const applicant_id = parseInt(params[2]);
+    const vote = parseInt(params[3]);
 
     lib.messagebird.tel.sms({
         originator: receiver, // (required)
         recipient: sender, // (required)
-        body: message // (required)
+        body: `Successfully received message: ${message}` // (required)
     }, (err, result) => {
-        updateVote(message, context, function(err, data) {
-            callback(null, JSON.stringify(data));
+        updateVote(listing_id, voter_id, applicant_id, vote, context, function(err, data) {
+            callback(null);
         });
     });
 };
