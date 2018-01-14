83a01fe9c80ba978d452fdf5b4bc19f01fd58f0d
Ziyang Jin <ziyang.jin.master@gmail.com>
2018-01-14 10:42:14 -0800

    Update user creation logic to auto compute new id

--- functions/createUser.js
+++ functions/createUser.js
@@ -2,6 +2,11 @@ const mLab = require('mongolab-data-api')('HHeYK9l4vyPr4X54kLnTa7-zZp0IHxD9');
 
 /**
  * Create a new user profile in MongoDB's Users table
+ * It first get the number of records of the table, then compute the new id = size + 1
+ * then wrap the user infomation into an object and add a new entry in users table
+ * 
+ * To test, type:
+ *     lib .createUser --message "newusername 1234567890" 
  *
  * @param {string} message Message to pass through
  * @returns {string}
@@ -9,25 +14,40 @@ const mLab = require('mongolab-data-api')('HHeYK9l4vyPr4X54kLnTa7-zZp0IHxD9');
  */
 module.exports = (message, context, callback) => {
 
+    // ****** assumption message "username phonenumber" *********
+    const params = message.split(' ');
+    const username = params[0];
+    const phone = parseInt(params[1]);
+
     const DB_NAME = 'roommates';
     const USERS = 'Users';
     const USERS_ID = 'id';
     const USERS_NAME = 'name';
     const USERS_PHONE = 'phone';
 
-    const newUser = {};
-    newUser[USERS_ID] = 3;
-    newUser[USERS_NAME] = 'Jon Kle';
-    newUser[USERS_PHONE] = 7783214512;
-
     const options = {
         database: DB_NAME,
         collectionName: USERS,
-        documents: newUser
+        resultCount: true
     };
 
-    mLab.insertDocuments(options, function (err, data) {
-        console.log("a new user has been inserted");
-        callback(null, message);
+    mLab.listDocuments(options, function (err, data) {
+        console.log("get data " + data);
+        const newUser = {};
+        newUser[USERS_ID] = data + 1;
+        newUser[USERS_NAME] = username;
+        newUser[USERS_PHONE] = phone;
+        const options = {
+            database: DB_NAME,
+            collectionName: USERS,
+            documents: newUser
+        };
+
+        mLab.insertDocuments(options, function (err, data) {
+            console.log("a new user has been inserted");
+            callback(null, message);
+        });
+
     });
+
 };
